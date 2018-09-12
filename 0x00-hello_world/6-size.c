d6479563d6b36e1a45098ae0b0b6d1c3a6474dbe
sazad44 <530@holbertonschool.com>
2018-09-12 23:29:00 +0000

    Uploading c file to display the byte sizes of different variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int n;
+	unsigned long int l;
+	long long int d;
+	float f;
+	printf("Size of a char: %u byte(s)\n", sizeof(c));
+	printf("Size of an int: %u byte(s)\n", sizeof(n));
+	printf("Size of a long int: %u byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(d));
+	printf("Size of a float: %u byte(s)\n", sizeof(f));
+	return (0);
+}
