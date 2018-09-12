9f7ad4990f6960ff5d88a303e9f292d59439118b
sazad44 <530@holbertonschool.com>
2018-09-12 22:05:37 +0000

    Updated task 4 C file to have a description for the main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * Main is initialized as an integer returning function and it prints a specific
+ * shown below in the puts command.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
