d7d5c1f01c4c45a6a07403b42d82114c7dfad4e4
sazad44 <530@holbertonschool.com>
2018-09-13 23:32:42 +0000

    Uploading c file for task 2 to print the lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a';  n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
