84bfbdba4b4b9b2ffb153442d1dbba47612c3109
sazad44 <530@holbertonschool.com>
2018-09-18 18:19:32 +0000

    Uploading task 11 c file to print numbers from an initial point to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,33 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (;  n >= 98; n--)
 		{
-			printf("%d, ", n);
-			_putchar('\n');
+			printf("%d", n);
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+		putchar('\n');
 	}
 	else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+	else
+	{
+		printf("%d", n);
+		putchar('\n');
+	}
+}
