e37289aa71e8b9e9a7d1a46e93f49bb1d8cb5757
sazad44 <530@holbertonschool.com>
2018-09-19 01:22:25 +0000

    Updating holberton header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,6 @@
-#include <unistd.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -30,3 +24,7 @@ void times_table(void);
 int add(int, int);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
