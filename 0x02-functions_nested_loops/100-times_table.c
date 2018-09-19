05a23695f63f6272e9137dc923f390ca311ca39e
sazad44 <530@holbertonschool.com>
2018-09-19 04:12:34 +0000

    Uploading c file to print out times tables based on user input

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,43 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
+	int x;
+	int y;
+	int z;
+
+	if (n > 0 && n < 15)
 	{
-		int i;
-		int x;
-		int y;
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+			for (y = 1; y <= n; y++)
+			{
+				z = (x * y);
+				if (((z % 100) % 10) < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (z >= 100)
+				{
+					_putchar((z / 100) + '0');
+					_putchar(((z % 100) / 10) + '0');
+					_putchar(((z % 100) % 10) + '0');
+				}
+				else if (z >= 10)
+				{
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
