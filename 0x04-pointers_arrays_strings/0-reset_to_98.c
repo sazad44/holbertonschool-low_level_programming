ac1581ccd1383889f1826a1b0e1fa5cd84635aa0
sazad44 <530@holbertonschool.com>
2018-09-24 18:52:49 +0000

    Uploading c file for task 0 to reset pointer's dereferenced value to 98 and updating holberton header file

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - converts a pointer's dereferenced value to 98
+ * @n: a pointer accepted to access and change the value of
+ * Return: no value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
