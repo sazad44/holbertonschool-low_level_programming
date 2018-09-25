b8f7c13f97696730a6c055d1dd38e4508924fa09
sazad44 <530@holbertonschool.com>
2018-09-25 17:30:58 +0000

    Uploading task 8 c file to print a certain number of elements in an array and updating holberton header file

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - print a certain number of elements in an array
+ * @a: an array input to be printed
+ * @n: number of elements of the array to be printed
+ * Return: no value
+ */
+void print_array(int *a, int n)
+{
+	int j;
+
+	n -= 1;
+	for (j = 0; j <= n; j++)
+	{
+		printf("%d", a[j]);
+		if (j != n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
