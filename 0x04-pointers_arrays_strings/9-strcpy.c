18d9b29e6f3cdde842f7ad530bc18b8de8156018
sazad44 <530@holbertonschool.com>
2018-09-25 17:55:34 +0000

    Uploading task 9 c file to copy a string to another string and return it, also updating holberton header file

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copy string pointed to by second input to the first input
+ * @dest: the buffer to have a string copied to
+ * @src: the string to copy to dest
+ * Return: the pointer dest's value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0, i = 0;
+
+	while (src[n])
+	{
+		n++;
+	}
+	while (i <= n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
