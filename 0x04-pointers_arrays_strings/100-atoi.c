f5e3eae3109f4f70e26c8408cfc4d5369e3438a1
sazad44 <530@holbertonschool.com>
2018-09-26 00:18:19 +0000

    Uploading task 10 c file to convert string to integer and updating holberton header file

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert a string into an integer
+ * @s: a string input to be converted into an integer
+ * Return: an integer developed from the string
+ */
+int _atoi(char *s)
+{
+	unsigned int n = 0, neg = 0, isnum = 0, isalpha = 0, res = 0;
+
+	neg = 0;
+	while (s[n])
+	{
+		if (s[n] == '-')
+		{
+			neg += 1;
+		}
+		if (s[n] >= '0' && s[n] <= '9')
+	       	{
+	       		res = res * 10 + (s[n] - '0');
+       			isnum = 1;
+       		}
+       		if (!(s[n] >= '0' && s[n] <= '9'))
+       		{
+			if (isnum == 1)
+				isalpha = 1;
+       		}
+		if (isnum == 1 && isalpha == 1)
+			break;
+		n++;
+       	}
+	if (neg % 2 != 0)
+		res = -res;
+	return (res);
+}
