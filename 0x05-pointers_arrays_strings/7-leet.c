309f6ea4292f7a1acecd7ac0121891e976d6a151
sazad44 <530@holbertonschool.com>
2018-09-28 01:39:45 +0000

    Uploading task 7 c file to print a string in leet and updating holberton header file

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * leet - converts a string into leet speak
+ * @targ: the string to be converted into leet speak
+ * Return: a string's pointer
+ */
+char *leet(char *targ)
+{
+	int i = 0, num[5] = {4, 3, 0, 1, 7}, j = 0;
+	char alpha[10] = "AaEeOoLlTt";
+
+	for (; targ[i]; i++)
+	{
+		for (j = 0; (targ[i] != alpha[j] && j < 10); j++)
+			;
+		if (targ[i] == alpha [j])
+		{
+			j /= 2;
+			targ[i] = (num[j] + '0');
+		}
+	}
+	return (targ);
+}
