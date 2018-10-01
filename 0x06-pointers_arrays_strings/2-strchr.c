406229282126b95cda6f66136432f099c400b377
sazad44 <530@holbertonschool.com>
2018-10-01 19:18:40 +0000

    Uploading task 2 c file to locate a character in a string and print the rest and updating holberton header file

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to be searched through
+ * @c: the character to find
+ * Return: the string starting with the character found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			p = (s + i);
+			return (p);
+		}
+	}
+	return ("NULL");
+}
