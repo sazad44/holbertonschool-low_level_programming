5f96d4a8b2bb97ae2d1cd759edb0fe2ba22aaee3
sazad44 <530@holbertonschool.com>
2018-10-02 00:35:39 +0000

    Uploading task 5 c file to find the first occurrence of a substring in a string and updating holberton header file

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds the first occurrence of substring
+ * @haystack: the string to be analyzed
+ * @needle: the string to be found
+ * Return: a pointer to the substring beginning
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, flag = 0;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j]; j++)
+			{
+				if (haystack[i + j] == needle[j])
+					flag = 1;
+				else
+					break;
+			}
+		}
+		if (flag == 1)
+			return ((haystack + i));
+	}
+	return (0);
+}
