74f9173adc72de1b3042c9e9f418b1aeea868630
sazad44 <530@holbertonschool.com>
2018-10-03 17:11:16 +0000

    Uploading task 2 c file to measure length of a string input and updating holberton header file

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -5,3 +5,11 @@
  * @s: the string to be measured
  * Return: an integer that represents the length of the string
  */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s++)
+		count += (1 + _strlen_recursion(s));
+	return (count);
+}
