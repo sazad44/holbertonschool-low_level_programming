623e0a99fbe577f580950d205a98c7ec7847a581
sazad44 <530@holbertonschool.com>
2018-10-06 16:09:29 +0000

    Updating task 1 c file to print the number of arguments passed to it and conform to betty style requirements

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- *
+ * @argc: the number of arguments passed to the program including its name
+ * @argv: a pointer to a pointer to the array of arguments passed
  * Return: ALways 0 (Success)
  */
 int main(int argc, char *argv[])
