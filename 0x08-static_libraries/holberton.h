53a77746d965d2b754532a652bf621514e8c821f
sazad44 <530@holbertonschool.com>
2018-10-06 21:34:17 +0000

    Retrying to upload library archive of a group of functions and updating holberton header file

--- 0x08-static_libraries/holberton.h
+++ 0x08-static_libraries/holberton.h
@@ -108,30 +108,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
-
-void _puts_recursion(char *s);
-
-void _print_rev_recursion(char *s);
-
-int _strlen_recursion(char *s);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int _sqrt_helper(int n, int x);
-
-int is_prime_number(int n);
-
-int is_prime_helper(int n, int x);
-
-int is_palindrome(char *s);
-
-int is_pal_helper(char *s, int x, int len);
-
-int measure(char *s);
-
-void _puts(char *s);
 #endif
