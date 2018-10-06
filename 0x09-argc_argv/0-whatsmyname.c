e34ca6bb13fbb9c79c7b5c67ee327c2ae8cee119
sazad44 <530@holbertonschool.com>
2018-10-06 16:07:07 +0000

    Updating task 0 c file to print the name of the program and conform to style rules

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- *
+ * @argc: the number of arguments passed to the program including its own name
+ * @argv: a pointer to the pointer to the array of arguments
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
