d1df66bf4444f7b69148d5bc6331fdf6e21a3244
sazad44 <530@holbertonschool.com>
2018-10-09 19:27:19 +0000

    Modifying task 0 c file to deal with errors more clearly

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
 	p = malloc(sizeof(char) * size);
+	if (size == 0 || p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
