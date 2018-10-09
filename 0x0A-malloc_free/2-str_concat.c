db79ab796de9c98f980fce69e7b8facf994812a9
sazad44 <530@holbertonschool.com>
2018-10-09 20:01:14 +0000

    Uploading task 2 c file to concatenate two strings and updating holberton header file

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * str_concat - concatenate two strings
+ * @s1: the first string to be concatenated
+ * @s2: the second string to be concatenated
+ * Return: a pointer to the new memory holding the concatenated strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i, sizes1 = _strlen(s1), sizes2 = _strlen(s2);
+
+	p = malloc(sizeof(char) * (sizes1 + sizes2));
+	if (p == )
+		return (0);
+	for (i = 0; *s1; i++)
+	{
+		p[i] = *s1;
+		s1++;
+	}
+	for (; *s2; i++)
+	{
+		p[i] = *s2;
+		s2++;
+	}
+	return (p);
+}
+
+/**
+ * strlen - measure a string
+ * @s: string to be measured
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return(i);
+}
--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -112,4 +112,10 @@ void set_string(char **s, char *to);
 char *create_array(unsigned int size, char c);
 
 char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+char *strinsert(char *a, char *p);
+
+int **alloc_grid(int width, int height);
 #endif
