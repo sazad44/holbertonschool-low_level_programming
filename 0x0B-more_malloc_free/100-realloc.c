64bca47a562b17b6a32afafb6b0da0c4afd740c3
sazad44 <530@holbertonschool.com>
2018-10-11 23:21:49 +0000

    Updating task 4 c file to deal with a new size of 0 more correctly

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -23,7 +23,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ret);
 	}
 	else if (new_size == 0)
+	{
 		free(ptr);
+		return (NULL);
+	}
 	ret = malloc(new_size);
 	if (ret == NULL)
 		return (NULL);
