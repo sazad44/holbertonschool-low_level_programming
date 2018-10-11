b9c396a1047d958e75b993ecf06082909f7fb6b2
sazad44 <530@holbertonschool.com>
2018-10-11 19:25:57 +0000

    Uploading task 2 c file to initialize an array of a certain size with 0 and updating holberton header file

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of elements in the array
+ * @size: size of the members in bytes
+ * Return: a void pointer to the memory addresses of the array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ret;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ret = malloc(size * nmemb);
+	if (ret == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		*((char *)ret + i) = 0;
+	}
+	return (ret);
+}
