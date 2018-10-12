47e547c52f0e955b6a419ac8b55856184e04f79e
sazad44 <530@holbertonschool.com>
2018-10-12 01:45:07 +0000

    Uploading task 5 c file for further work later

---
+++ 0x0B-more_malloc_free/101-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
