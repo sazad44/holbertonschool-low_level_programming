b3565a6fa19e190869ccacee00edd8d6aa658d16
sazad44 <530@holbertonschool.com>
2018-10-15 20:48:42 +0000

    Updating task 2 c file to print the correct number of places after the decimal for age

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,14 +10,14 @@
 void print_dog(Dog *d)
 {
 	if (d == NULL)
-		printf("\n");
+		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	if (d->age >= 0)
-		printf("Age: %0.5f\n", d->age);
+		printf("Age: %0.6f\n", d->age);
 	else
 		printf("Age: (nil)\n");
 	printf("Owner: %s\n", d->owner);
