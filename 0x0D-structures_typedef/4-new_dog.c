61dc5bc22c87bdcb27b2067328aac0dfdd2c38bf
sazad44 <530@holbertonschool.com>
2018-10-15 23:51:37 +0000

    Updating task 4 c file to initialize a new structure

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,36 +11,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ndog = {0};
+	dog_t *ndog;
 	char *nam, *own;
 	int i = 0, lenn = 0, leno = 0;
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-	while (name[i++])
-		lenn++;
-	i = 0;
-	while (owner[i++])
-		leno++;
-	i = 0;
+	if (name != NULL && owner != NULL)
+	{
+		while (name[i++])
+			lenn++;
+		i = 0;
+		while (owner[i++])
+			leno++;
+		i = 0;
+	}
 	nam = malloc(sizeof(char) * (lenn + 1));
 	own = malloc(sizeof(char) * (leno + 1));
 	if (nam == NULL || own == NULL)
+	{
+		free(ndog);
+		free(nam);
+		free(own);
 		return (NULL);
+	}
 	while (name[i])
 	{
 		nam[i] = name[i];
 		i++;
 	}
+	nam[i] = name[i];
 	i = 0;
 	while (owner[i])
 	{
 		own[i] = owner[i];
 		i++;
 	}
+	own[i] = owner[i];
 	ndog->name = nam;
 	ndog->age = age;
 	ndog->owner = own;
