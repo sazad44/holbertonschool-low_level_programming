d0b53c5b39385be65cc32e393c3a2b5e4ab82c5b
sazad44 <530@holbertonschool.com>
2018-10-15 19:36:01 +0000

    Uploading task 0 header file to create a structure dog with attributes

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef doge
+#define doge
+/**
+ * struct dog - a structure of the attributes of dogs
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
+#endif
