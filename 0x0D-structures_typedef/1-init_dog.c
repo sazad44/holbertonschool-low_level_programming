cbc5a5ed56f4110799a1a200f8051418cbac1369
sazad44 <530@holbertonschool.com>
2018-10-15 20:19:42 +0000

    Updating task 1 c file to handle empty value inputs and updating header file

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -9,6 +11,12 @@
  */
 void init_dog(Dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(Dog));
+		if (d == NULL)
+			return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
