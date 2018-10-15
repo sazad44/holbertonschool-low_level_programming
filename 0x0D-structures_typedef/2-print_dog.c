c1d83d6d670f3ffa53333933c2d7404ad52b5972
sazad44 <530@holbertonschool.com>
2018-10-15 20:40:12 +0000

    Uploading task 2 c file to print out a structure and updating header file

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a structure dog
+ * @d: the structure to print
+ * Return: no value
+ */
+void print_dog(Dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	if (d->age >= 0)
+		printf("Age: %0.5f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	printf("Owner: %s\n", d->owner);
+}
