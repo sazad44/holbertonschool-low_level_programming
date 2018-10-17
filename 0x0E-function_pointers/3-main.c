b74b6c7e2d425d50e7e5b67aff818c269384cc47
sazad44 <530@holbertonschool.com>
2018-10-17 20:41:31 +0000

    Updating task 3 c file to check for a length of 1 in the operator input

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -4,7 +4,8 @@
 
 /**
  * main - Entry point
- *
+ * @argc: the number of arguments including the program name passed
+ * @argv: a pointer to pointers to the strings submitted as arguments
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
@@ -15,17 +16,17 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	if (get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
