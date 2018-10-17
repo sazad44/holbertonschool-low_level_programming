175af9401057f57d90fcab2c61f18239eb3ea69f
sazad44 <530@holbertonschool.com>
2018-10-17 17:12:32 +0000

    Uploading task 2 c file to search for an integer in an array

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - write a function that searches for an integer
+ * @array: the array to search for an integer in
+ * @size: size of the array
+ * @cmp: pointer to a function to compare values
+ * Return: the index of the first elements that cmp doesnt return 0 for
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
