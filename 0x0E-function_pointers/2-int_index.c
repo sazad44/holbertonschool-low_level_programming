0204845e8b94a39dcf10ac679232cf2e3946eb6c
sazad44 <530@holbertonschool.com>
2018-10-17 19:38:51 +0000

    Updating task 2 c file to deal with error inputs more accurately

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * int_index - write a function that searches for an integer
@@ -12,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return (0);
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
