6beb70dbcd14b73eb37c3442af354ff9e827bd3e
sazad44 <530@holbertonschool.com>
2018-10-17 19:09:51 +0000

    Updating task 1 c file to deal with empty parameter entries

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "function_pointers.h"
 
@@ -12,6 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || size == 0 || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
