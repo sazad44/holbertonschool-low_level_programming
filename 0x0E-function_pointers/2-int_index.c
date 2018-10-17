813578c6db973f6458a225012d639d967ad1d02a
sazad44 <530@holbertonschool.com>
2018-10-17 19:12:03 +0000

    Updating task 2 c file to deal with empty parameter inputs

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -11,6 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || size == 0 || cmp == NULL)
+		return (0);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
