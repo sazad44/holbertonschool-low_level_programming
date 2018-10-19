b828a1a4d713da3e7ace3095eef72edcad13b120
sazad44 <530@holbertonschool.com>
2018-10-19 18:21:59 +0000

    Uploading task 1 c file to print numbers and updating header file

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @seperator: character pointer that points to the separator string
+ * @n: the number inputs to be printed
+ * Return: No Value
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numtoprint;
+
+	va_start(numtoprint, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numtoprint, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(numtoprint);
+}
