21a34fdf7eb12d2310406bef06041fb00f9ec929
sazad44 <530@holbertonschool.com>
2018-10-19 19:09:48 +0000

    Uploading task 2 c file to print strings passed as parameters and updating header file

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: the string to be printed between the strings
+ * @n: the number of strings passed to the function
+ * Return: No Value
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list strtopr;
+	char *sub;
+
+	va_start(strtopr, n);
+	for (i = 0; i < n; i++)
+	{
+		sub = va_arg(strtopr, char *);
+		if(sub == NULL)
+			printf("(nil)");
+		else
+			printf("%s", sub);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
