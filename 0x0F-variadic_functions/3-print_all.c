41dcd01f8facc08b99a923953f4d49e389668744
sazad44 <530@holbertonschool.com>
2018-10-20 07:39:19 +0000

    Uploading task 3 file for continued work and updating header file

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -4,33 +4,82 @@
 #include <stdarg.h>
 
 /**
+ * prt_char - prints a character pointed to by a va_list
+ * @ch: the character to be printed
+ * Return: No Value
+ */
+void prt_char(va_list ch)
+{
+	printf("%s", va_arg(ch, char *));
+}
+
+/**
+ * prt_int - prints an integer pointed to by a va_list
+ * @in: the integer to be printed
+ * Return: No Value
+ */
+void prt_int(va_list in)
+{
+	printf("%d", va_arg(in, int));
+}
+
+/**
+ * prt_float - prints a float pointed to by a va_list
+ * @fl: the float to be printed
+ * Return: No Value
+ */
+void prt_float(va_list fl)
+{
+        printf("%f", va_arg(fl, double));
+}
+
+/**
+ * prt_str - prints a string pointed to by a va_list
+ * @st: the string to be printed
+ * Return: No Value
+ */
+void prt_str(va_list st)
+{
+        printf("%s", va_arg(st, char *));
+}
+
+/**
  * print_all - prints anything
  * @format: a pointer to a list of types being fed into the function
  * Return: No Value
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j = 0, len = 0;
+	int i = 0, j = 0;
 	va_list elemtopr;
 	typedef struct var
 	{
 		char *c;
-		void (*prf)(char *);
+		void (*prf)(va_list);
 	}var;
 	var varray[] = {
-		{'c', prf_char},
-		{'i', prf_int},
-		{'f', prf_float},
-		{'s', prf_str}
+		{"c", prt_char},
+		{"i", prt_int},
+		{"f", prt_float},
+		{"s", prt_str},
 		{NULL, NULL}
-	}
+	};
+	void *sub;
+	va_start(elemtopr, format);
 	while (format[i])
 	{
-		if (format[i] == *(varray[j].c))
+		sub = va_arg(elemtopr, void *);
+		while (varray[j].c)
 		{
-			len++;
-			i++;
+			if (format[i] == *(varray[j].c))
+				printf("hello");
+				varray[j].prf(sub);
+			j++;
 		}
-		j++;
+		if ((format + 1) != NULL)
+			printf(", ");
+		i++;
 	}
+	va_end(elemtopr);
+	printf("\n");
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -11,23 +11,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
-void prt_char(char *ch)
-{
-	printf("%s", ch);
-}
-
-void prt_int(int in)
-{
-	printf("%d", in);
-}
-
-void prt_float(double *fl)
-{
-	printf("%f", fl);
-}
-
-void prt_str(char *st)
-{
-	printf("%s", st);
-}
 #endif
