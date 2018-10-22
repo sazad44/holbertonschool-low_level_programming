8dbb3bb1843f647efed3c5133d7b184a8d1846c0
sazad44 <530@holbertonschool.com>
2018-10-22 06:36:59 +0000

    Updating task 4 assembly file to not print a null byte

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -5,11 +5,11 @@ main:				; This is called by the C library startup code
 	          mov       	rax, 1 ; First integer (or pointer) argument in rdi
 	          mov		rdi, 1; puts(message)
 	          mov		rsi, message ; Return from main back into C library wrapper
-		  mov rdx, 18
+		  mov rdx, 17
 		  syscall
 		  mov rax ,60
 		  xor rdi, rdi
 		  syscall
 		section .data
 message:
-	          db        "Hello, Holberton", 0, 10 ; Note strings must be terminated with 0 in C
+	          db        "Hello, Holberton", 10 ; Note strings must be terminated with 0 in C
