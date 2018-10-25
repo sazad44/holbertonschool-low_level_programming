cab026bce5ee57315c2ed1e1cfbf0a29acae5bc2
sazad44 <530@holbertonschool.com>
2018-10-25 20:29:55 +0000

    Uploading task 3 c file for further development to add a node to a linked list and updating header file

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a new node at the end of the linked list
+ * @head: a pointer to a pointer to the head struct fed in
+ * @str: the str fed in to add to the newly appended struct
+ * Return: a pointer to the first struct in the new linked list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	int len;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	for (len = 0; str[len]; len++);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+	(*head)->next = new;
+	return (*head);
+}
