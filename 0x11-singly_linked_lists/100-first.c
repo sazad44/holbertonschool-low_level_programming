cbe0578a4a7bb821dfbc7b6150c33a6779c9c32b
sazad44 <530@holbertonschool.com>
2018-10-26 05:12:53 +0000

    Updated task 5 c file to be Betty style compliant

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
  */
 void but_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my\
-back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my ");
+	printf("house upon my back!\n");
 }
