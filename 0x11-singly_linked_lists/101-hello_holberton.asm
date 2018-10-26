c1f40495d399283991cf9057ee8085d19e5f65cd
sazad44 <530@holbertonschool.com>
2018-10-26 06:06:53 +0000

    Uploading task 6 assembly file to print a string

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+global    main
+	          extern    printf
+
+	          section   .text
+main:	push      rbx	; Call stack must be aligned
+	          lea       rdi, [rel message] ; First argument is address of message
+	          call      printf	       ; puts(message)
+	          pop       rbx		       ; Fix up stack before returning
+	          ret
+
+	          section   .data
+message:	  db        "Hello, Holberton", 10, 0
