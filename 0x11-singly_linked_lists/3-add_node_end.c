d53391db2b393f52949ae262792c88a54d0d7a3a
sazad44 <530@holbertonschool.com>
2018-10-26 03:54:06 +0000

    Updating task 3 c file with a working function to add a node to the end of a linked list and updating header file

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -8,16 +8,25 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *temp;
 	int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	for (len = 0; str[len]; len++);
+	for (len = 0; str[len]; len++)
+		;
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
-	(*head)->next = new;
-	return (*head);
+	if ((*head) == NULL)
+		(*head) = new;
+	else
+	{
+		temp = *head;
+		for (; temp->next;)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
 }
