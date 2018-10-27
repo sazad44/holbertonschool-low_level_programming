37a84dcfde153bfd25aa7a4bde45e5433f2fd80b
sazad44 <530@holbertonschool.com>
2018-10-27 00:00:29 +0000

    Updating assembly file to return success

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -4,8 +4,9 @@
 	          section   .text
 main:	push      rbx	; Call stack must be aligned
 	          lea       rdi, [rel message] ; First argument is address of message
-	          call      printf	       ; (message)
-	          pop       rbx		       ; Fix up stack before returning
+		  call      printf	       ; (message)
+		  xor 	    eax,eax
+		  pop       rbx		       ; Fix up stack before returning
 	          ret
 
 	          section   .data
