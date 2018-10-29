e7c265388ed076932d04272c9d85b21c362fb3f9
sazad44 <530@holbertonschool.com>
2018-10-29 22:53:42 +0000

    Uploading task 10 c file to delete a node from a linked list and updating header file

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete a node at a certain index in a linked list
+ * @head: a pointer to a pointer to the first struct in a linked list
+ * @index: the index at which to delete the node
+ * Return: 1 if SUCCESS or -1 if FAIL
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	int ret = -1;
+	unsigned int i;
+	listint_t *temp, *temp2;
+
+	if (*head == NULL)
+		return (ret);
+	else
+		ret = 1;
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	else
+	{
+		for (i = 0, temp = *head; i < index - 1; i++, temp = temp->next)
+			;
+		temp2 = temp;
+		temp = temp->next;
+		temp2->next = temp->next;
+		free(temp);
+	}
+	return (ret);
+}
