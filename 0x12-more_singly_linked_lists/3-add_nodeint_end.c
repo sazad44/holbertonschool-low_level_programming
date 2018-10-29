758c8e53ce1998be7c400fc1dc941aae6456b1a2
sazad44 <530@holbertonschool.com>
2018-10-29 17:13:30 +0000

    Uploading task 3 c file to add a new node to the end of a list and updating header file

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a new node to the end of the linked list
+ * @head: a pointer to a pointer to the head structure in the linked list
+ * @n: the number to integrate into the new  node of the linked list
+ * Return: the address of the new element in the linked list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *temp;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		new->n = n;
+		new->next = NULL;
+		for (temp = *head; temp->next; temp = temp->next)
+			;
+		temp->next = new;
+	}
+	return (new);
+}
