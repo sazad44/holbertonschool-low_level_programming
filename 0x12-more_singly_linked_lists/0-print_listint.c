f4b1faab3e27d825992193f2b2966f2a9b44369e
sazad44 <530@holbertonschool.com>
2018-10-29 16:37:22 +0000

    Uploading task 0 c file to print the elements of a list and return a count of them as well as updating header file

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of a linked list of structures
+ * @h: the pointer to the first element of the list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int num;
+
+	for (num = 0; h; num++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (num);
+}
