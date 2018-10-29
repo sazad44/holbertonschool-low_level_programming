152d4c774d80aba8e15ba87d827213f5b2481082
sazad44 <530@holbertonschool.com>
2018-10-29 21:09:27 +0000

    Uploading task 7 c file to return a specific node in a linked list and updating header file

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: a pointer to the beginning struct of the linked list
+ * @index: the index of the node to be returned
+ * Return: the pointer the number index structure of the linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; head = head->next, i++)
+		;
+	return (head);
+}
