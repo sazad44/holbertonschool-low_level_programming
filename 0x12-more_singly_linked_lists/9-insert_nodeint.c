d4d5d166e031572267983e4c574ad5351bc18855
sazad44 <530@holbertonschool.com>
2018-10-30 17:47:06 +0000

    Updating task 9 c file to deal with index of 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	for (i = 0, temp = *head; *head && (i < (idx - 1)); i++, temp = temp->next)
 		;
 	if (idx > i + 1 || *head == NULL)
