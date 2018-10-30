4dc4a44d1cf971c4f9ddc3acf36d1d6213182e1b
sazad44 <530@holbertonschool.com>
2018-10-30 17:13:51 +0000

    Updating task 7 c file to check for nonexistent node correctly

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,9 +10,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	for (i = 0; i < index; head = head->next, i++)
 		;
+	if (head == NULL)
+		return (NULL);
 	return (head);
 }
