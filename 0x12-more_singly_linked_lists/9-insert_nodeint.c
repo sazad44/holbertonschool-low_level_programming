8ea8c904b81aa097e4e8cce88a30d83fbcb189c3
sazad44 <530@holbertonschool.com>
2018-10-30 17:36:50 +0000

    Updating task 9 c file to deal with more NULL cases and not to iterate through a linked list past a NULL value

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodint_at_index - inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: a pointer to the pointer to the first struct in the linked list
  * @idx: the index at which the new element has to be added
  * @n: the piece of data to put into the new node
@@ -12,14 +12,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *temp, *temp2;
 	unsigned int i;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	for (i = 0, temp = *head; i < idx - 1; i++, temp = temp->next)
+	for (i = 0, temp = *head; *head && (i < (idx - 1)); i++, temp = temp->next)
 		;
-	if (idx > i + 1)
+	if (idx > i + 1 || *head == NULL)
 		return (NULL);
 	temp2 = temp->next;
 	temp->next = new;
