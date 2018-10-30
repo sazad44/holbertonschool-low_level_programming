4ff6c35e6263bb9db93fc99e5c7060e6ac83858a
sazad44 <530@holbertonschool.com>
2018-10-30 18:05:56 +0000

    Updating task 9 c file to deal with indexes out of range

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 0, temp = *head; *head && (i < (idx - 1)); i++, temp = temp->next)
 		;
-	if (idx > i + 1 || *head == NULL)
+	if (*head == NULL || idx > i)
+	{
+		free(new);
 		return (NULL);
+	}
 	temp2 = temp->next;
 	temp->next = new;
 	new->next = temp2;
