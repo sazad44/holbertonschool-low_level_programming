b629bd7166561a1ca10705bad67c0ec4c760c47d
sazad44 <530@holbertonschool.com>
2018-10-30 16:34:56 +0000

    Updating task 3 c file to create a new structure even if head is null

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,12 +13,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next = NULL;
 	if (*head == NULL)
 		*head = new;
 	else
 	{
-		new->n = n;
-		new->next = NULL;
 		for (temp = *head; temp->next; temp = temp->next)
 			;
 		temp->next = new;
