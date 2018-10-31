c60e313e07ab082cc60ec94b7cfcd834c5534322
sazad44 <530@holbertonschool.com>
2018-10-31 05:29:06 +0000

    Uploading task 12 c file for further work

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,12 +8,33 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	int num;
+	const listint_t *temp = head, *temp2 = head, *temp3 = head;
 
-	if (h == NULL)
+	if (head == NULL)
 		return (0);
-	for (num = 0; h; num++)
+	for (num = 0; head; num++)
 	{
-		printf("[%p] %d\n", head->next, head->n);
+		temp2 = temp2->next;
+		while (temp2)
+		{
+			if (temp2 == temp)
+			{
+				temp2 = temp3;
+				while (temp != temp2)
+				{
+					temp2 = temp2->next;
+					temp2 = temp2->next;
+					temp = temp->next;
+				}
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (num);
+			}
+			temp2 = temp2->next;
+			if (temp2)
+				temp2 = temp2->next;
+			temp = temp->next;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
 	return (num);
