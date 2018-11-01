9452574c94f57f0d8054853f3d45555209174f48
sazad44 <530@holbertonschool.com>
2018-11-01 19:50:07 +0000

    Updating task 2 c file to deal with too large indexes

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -8,9 +8,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i, j, div;
 
+	for (j = 0, div = 1; div <= n; j++, div *= 2)
+		;
 	for (i = 0; i < index; i++)
 		n >>= 1;
+	if (i > j - 1)
+		return (-1);
 	return (n & 1);
 }
