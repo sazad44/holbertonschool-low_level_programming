15ca238190653b9a65c28700cd60a34c429c3177
sazad44 <530@holbertonschool.com>
2018-11-06 22:58:43 +0000

    Uploading task 1 c file to create a file with a certain text and updating header file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: pointer to the file name
+ * @text_content: a pointer to the content to be inserted into the file
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = 0;
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd < 0)
+			return (-1);
+		close(fd);
+		return (1);
+	}
+	if (filename == NULL)
+		return (-1);
+	while (text_content[len++])
+		;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	wr = write(fd, text_content, len);
+	if (wr < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
