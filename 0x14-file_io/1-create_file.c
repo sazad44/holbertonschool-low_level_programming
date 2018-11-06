a83940820a8bddd95ae126348aa9ed8386980e9a
sazad44 <530@holbertonschool.com>
2018-11-06 23:05:37 +0000

    Updating task 1 c file to change order of NULL checks

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,19 +10,19 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, len = 0;
 
+	if (filename == NULL)
+		return (-1);
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 		if (fd < 0)
 			return (-1);
 		close(fd);
 		return (1);
 	}
-	if (filename == NULL)
-		return (-1);
 	while (text_content[len++])
 		;
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd < 0)
 		return (-1);
 	wr = write(fd, text_content, len);
