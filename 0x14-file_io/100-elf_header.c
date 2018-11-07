71831118447560bf315c90bf40f8ecda324e3fd5
sazad44 <530@holbertonschool.com>
2018-11-07 07:52:31 +0000

    Updated task 4 c file to compile

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -10,8 +10,9 @@ int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename");
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
 		exit(98);
 	}
+	(void)argv;
 	return (0);
 }
