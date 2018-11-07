adb0e40056322baeb1e18a8c1fab8efface3e0c3
sazad44 <530@holbertonschool.com>
2018-11-07 05:19:15 +0000

    Updating task 3 c file to copy one file into another successfully

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2;
+	int fd1, fd2, cl1, cl2, rd;
 	char *buf;
 
 	if (argc != 3)
@@ -23,9 +23,36 @@ int main(int argc, char *argv[])
 		printf("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_RDONLY | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 < 0)
 	{
 		printf("Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+	do
+	{
+		rd = read(fd1, buf, 1024);
+		if (rd < 0)
+		{
+			close(fd1);
+			close(fd2);
+			free(buf);
+			return (-1);
+		}
+		dprintf(fd2, "%s", buf);
+	} while (rd == 1024);
+	free(buf);
+	cl1 = close(fd1);
+	if (cl1 < 0)
+	{
+		printf("Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	cl2 = close(fd2);
+	if (cl2 < 0)
+	{
+		printf("Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (1);
+}
