f8654b9cf4820fbbd6e7c82d4aeee4e681005b2e
sazad44 <530@holbertonschool.com>
2018-11-07 00:27:25 +0000

    Updating task 1 c file with order change

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 00600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (fd < 0)
 		return (-1);
 	if (text_content && *text_content)
