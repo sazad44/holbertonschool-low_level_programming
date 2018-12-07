acdbd5b5f3e05733aa6e26878c50b20e56ebc369
sazad44 <530@holbertonschool.com>
2018-12-07 20:58:52 +0000

    Uploading task 3 c file to add nodes to a doubly linked list at the end and updating header

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add a new node to the end of a doubly linked list
+ * @head: a pointer to a pointer to the first node in the list
+ * @n: the number value to be put into the new node
+ * Return: the address of the new element or NULL if it fails
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *trav;
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		for (trav = *head; trav->next; trav = trav->next)
+			;
+		trav->next = new;
+		new->prev = trav;
+	}
+	return (new);
+}
