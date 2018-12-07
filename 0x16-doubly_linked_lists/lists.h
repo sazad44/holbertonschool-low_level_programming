b4d0cb2b7b91636f58d6c6a77357de3c3c767deb
sazad44 <530@holbertonschool.com>
2018-12-07 20:44:45 +0000

    Uploading header file

---
+++ 0x16-doubly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct dlistint_t{
+	int n;
+	struct dlistint_t *next;
+	struct dlistint_t *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+#endif
