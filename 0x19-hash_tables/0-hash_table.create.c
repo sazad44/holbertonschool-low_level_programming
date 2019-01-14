4cd009f144e53f01e6e6e009479064bd5f13f94c
sazad44 <530@holbertonschool.com>
2019-01-14 17:29:16 +0000

    Uploading task 0 c file to create hash table along with header

---
+++ 0x19-hash_tables/0-hash_table.create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table based on size provided
+ * @size: the size of the hash table to be initialized
+ * Return: a pointer to the start of the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ret = NULL;
+
+	if (size > 0)
+	{
+		ret = malloc(sizeof(hash_table_t));
+		if (ret == NULL)
+			return (NULL);
+		ret->size = size;
+		ret->array = malloc(sizeof(hash_node_t) * size);
+		if (ret->array == NULL)
+			return (NULL);
+		*(ret->array) = NULL;
+	}
+	else
+		return (NULL);
+	return (ret);
+}
