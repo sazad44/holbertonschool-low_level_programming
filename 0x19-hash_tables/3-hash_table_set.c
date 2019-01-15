271d99341fc79f174c3354ae247b9472ddce4f22
sazad44 <530@holbertonschool.com>
2019-01-15 20:40:42 +0000

    Updating task 3 c file's betty style compliance

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->next = NULL;
 	if (ht->array[ofs])
 	{
-	        temp = ht->array[ofs];
+		temp = ht->array[ofs];
 		ht->array[ofs] = new_node;
 		ht->array[ofs]->next = temp;
 	}
