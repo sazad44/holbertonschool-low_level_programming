6b2f1886885b5802d0a2a9206a5b2f5a629fa156
sazad44 <530@holbertonschool.com>
2019-01-15 20:39:12 +0000

    Updating task 3 c file to correctly handle collisions and overwrites

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -10,28 +10,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int ofs = 0;
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node = NULL, *temp = NULL;
 
-	if (!key || !strcmp(key, ""))
+	if (!ht || !key || !value || !strcmp(key, ""))
 		return (0);
 	ofs = (hash_djb2((const unsigned char *)key) % ht->size);
+	if (ht->array[ofs] && !strcmp(ht->array[ofs]->key, key))
+	{
+		if (ht->array[ofs]->value)
+			free(ht->array[ofs]->value);
+		ht->array[ofs]->value = strdup(value);
+		return (1);
+	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL || !ht || !value)
+	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
-	if (ht->array[ofs] && !strcmp(ht->array[ofs]->key, key))
+	if (ht->array[ofs])
 	{
-		ht->array[ofs]->value = new_node->value;
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-	}
-	else if (ht->array[ofs] && ht->array[ofs]->value)
-	{
-		new_node->next = ht->array[ofs];
+	        temp = ht->array[ofs];
 		ht->array[ofs] = new_node;
+		ht->array[ofs]->next = temp;
 	}
 	else
 		ht->array[ofs] = new_node;
