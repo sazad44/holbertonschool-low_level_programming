7b5555a0d99f8686fedd838717dc04cce40db582
sazad44 <530@holbertonschool.com>
2019-01-15 18:54:33 +0000

    Updating task 3 c file to handle empty keys and update dictionary values instead of add nodes

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,14 +12,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int ofs = 0;
 	hash_node_t *new_node = NULL;
 
+	if (!strcmp(key, ""))
+		return (0);
 	ofs = (hash_djb2((const unsigned char *)key) % ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
-	if (ht->array[ofs] && ht->array[ofs]->value)
+	if (ht->array[ofs] && !strcmp(ht->array[ofs]->key, key))
+	{
+		ht->array[ofs]->value = new_node->value;
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+	}
+	else if (ht->array[ofs] && ht->array[ofs]->value)
 	{
 		new_node->next = ht->array[ofs];
 		ht->array[ofs] = new_node;
