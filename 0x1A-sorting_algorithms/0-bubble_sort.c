6095e79b70faef1ada0a1f9d7f0337f5ae707eb1
sazad44 <530@holbertonschool.com>
2019-02-04 21:50:54 +0000

    Uploading task 1 c file for insertion sort function and updating betty style for task 0 c file

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,6 +1,10 @@
 #include "sort.h"
 
 /**
+ * bubble_sort - sorting a random array of integers with the bubble method
+ * @array: a pointer to the first piece of data of the array
+ * @size: the size of the array
+ * Return: No Value
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -18,6 +22,13 @@ void bubble_sort(int *array, size_t size)
 	}
 }
 
+/**
+ * swap - checks for unordered pairs and swaps to correct position
+ * @array: a pointer to the first element of the array
+ * @i: the position in the array being checked
+ * @size: the size of the array
+ * Return: Integer 1 if swap and 0 if not
+ */
 int swap(int *array, int i, size_t size)
 {
 	int tmp;
