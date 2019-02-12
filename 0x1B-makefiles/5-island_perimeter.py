5bfd420bd2f099e28d376b33962306c208870fdc
sazad44 <530@holbertonschool.com>
2019-02-12 01:34:40 +0000

    Uploading task 5 python module to check the perimeter of an island in a matrix

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""5-island-perimeter module"""
+
+
+def island_perimeter(grid):
+    """island_perimeter function"""
+    if type(grid) != list or len(grid) == 0:
+        return 0
+    per = 0
+    for i in range(len(grid)):
+        if type(grid[i]) != list:
+            return 0
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                per += per_check(grid, i, j)
+    return per
+
+def per_check(grid, i, j):
+    """per_check function"""
+    per = 0
+    try:
+        if grid[i][j - 1] == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    try:
+        if grid[i - 1][j] == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    try:
+        if grid[i + 1][j] == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    try:
+        if grid[i][j + 1] == 0:
+            per += 1
+    except IndexError:
+        per += 1
+    finally:
+        return per
