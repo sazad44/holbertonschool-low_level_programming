179c1d48fcab9ea7efbc0f6cf6b3cb491b684747
sazad44 <530@holbertonschool.com>
2019-03-13 05:34:05 +0000

    Uploading task 9 modifications for later work

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t hl = 0, hr = 0;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 	hl = binary_tree_height(tree->left) + 1;
 	hr = binary_tree_height(tree->right) + 1;
