0be1bed9b5c3852cf288cffdd5e65d38a75efd5b
sazad44 <530@holbertonschool.com>
2019-03-13 20:49:54 +0000

    Updating task 16 c file to handle more edge cases of imperfect binary trees

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -13,10 +13,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 	{
-		if (tree->left && tree->right)
-			return (1);
 		l = tree->left;
 		r = tree->right;
+		if ((!l && !r) || (l && r))
+			return (1);
 		return (binary_tree_is_perfect(l) && binary_tree_is_perfect(r));
 	}
 	return (0);
