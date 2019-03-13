b83bf3124266049a7be4402936e37933490c94a8
sazad44 <530@holbertonschool.com>
2019-03-13 16:15:56 +0000

    Updating task 15 c file to correctly determine if a binary tree is full

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -7,12 +7,11 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree && ((tree->left && tree->right) || (!tree->left && !tree->right)))
-	{
-		binary_tree_is_full(tree->left);
-		binary_tree_is_full(tree->right);
-	}
-	else
+	if (!tree)
 		return (0);
-	return (1);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree && (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right)))
+		return (1);
+	return (0);
 }
