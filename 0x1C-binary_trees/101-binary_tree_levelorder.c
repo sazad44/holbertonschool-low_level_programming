85e931bac4ffb70e508979b1874db0e454f5d5b0
sazad44 <530@holbertonschool.com>
2019-03-14 14:48:02 +0000

    Updating task 20 c file to handle all cases

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -8,14 +8,47 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
+	size_t treeHeight = 0, i = 0;
+
+	treeHeight = binary_tree_height(tree);
+	for (i = 1; i <= treeHeight; i++)
+		print_level(tree, i, func);
+}
+
+/**
+ * binary_tree_height - finds the height of a binary tree
+ * @tree: a pointer to the root node of a binary tree
+ * Return: an unsigned integer signifying the height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t* tree)
+{
+	size_t hl = 0, hr = 0;
+
+	if (!tree)
+		return (0);
+	hl = binary_tree_height(tree->left);
+	hr = binary_tree_height(tree->right);
+	if (hl > hr)
+		return (hl + 1);
+	else
+		return (hr + 1);
+}
+
+/**
+ * print_level - prints a level of a binary tree
+ * @tree: a pointer to the root node of the tree
+ * @level: the level to be printed
+ * Return: No Value
+ */
+void print_level(const binary_tree_t *tree, size_t level, void (*func)(int))
+{
+	if (!tree)
 		return;
-	else if (!tree->parent)
+	else if (level == 1)
 		func(tree->n);
-	if (tree->left)
-		func(tree->left->n);
-	if (tree->right)
-		func(tree->right->n);
-	binary_tree_levelorder(tree->left, func);
-	binary_tree_levelorder(tree->right, func);
+	else
+	{
+		print_level(tree->left, level - 1, func);
+		print_level(tree->right, level - 1, func);
+	}
 }
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -47,5 +47,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 bst_t *binary_trees_ancestor(const binary_tree_t *f, const binary_tree_t *s);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void print_level(const binary_tree_t *tree, size_t level, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
