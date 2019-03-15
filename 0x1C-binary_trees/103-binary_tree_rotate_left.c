1d9d0c7e89f91894a7ff1503e677d26589fcd87e
sazad44 <530@holbertonschool.com>
2019-03-15 22:05:54 +0000

    Updating task 22 file with correct left rotation algorithm as well as updating header file

--- 0x1C-binary_trees/103-binary_tree_rotate_left.c
+++ 0x1C-binary_trees/103-binary_tree_rotate_left.c
@@ -7,5 +7,26 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *tmpRoot = NULL;
 
+	if (!tree)
+		return (tree);
+	tmpRoot = tree;
+	tree = tree->right;
+	if (!tree->left)
+	{
+		tree->left = tmpRoot;
+		tmpRoot->right = NULL;
+	}
+	else
+	{
+		if (!tmpRoot->left)
+			tmpRoot->left = tree->left;
+		else
+			tmpRoot->right = tree->left;
+		tree->left = tmpRoot;
+	}
+	tmpRoot->parent = tree;
+	tree->parent = NULL;
+	return (tree);
 }
