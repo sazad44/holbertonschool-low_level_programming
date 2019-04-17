8daf64a251f0ea5b8e670d6691e67d1990457092
sazad44 <530@holbertonschool.com>
2019-04-17 04:11:51 +0000

    Updating task 7 c file with null array check

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -11,6 +11,8 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t idx = 0, jStep = sqrt(size), jSum = 0;
 
+	if (!array)
+		return (-1);
 	for (idx = 0; jStep <= size; jSum += jStep)
 	{
 		if (array[jSum] >= value || jSum > size)
