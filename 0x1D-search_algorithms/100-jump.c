6f3d83512109c5c86ee66cf4f84e19bac691d090
sazad44 <530@holbertonschool.com>
2019-04-17 21:58:57 +0000

    Updating task 7 c file to handle size of 0

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -11,7 +11,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t idx = 0, jStep = sqrt(size), jSum = 0;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 	for (idx = 0; jStep <= size; jSum += jStep)
 	{
