231ebc7d7ba3ce5091cc53ed04b752a921153e84
sazad44 <530@holbertonschool.com>
2019-04-19 07:12:54 +0000

    Updating task 12 c file for jump linked

--- 0x1D-search_algorithms/105-jump_list.c
+++ 0x1D-search_algorithms/105-jump_list.c
@@ -9,32 +9,31 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	unsigned char a = 0;
 	listint_t *tmpList = NULL, *idxList = NULL;
-	size_t jStep = sqrt(size), jSum = 0, idx = 0;
+	size_t jStep = sqrt(size), outIdx = 0;
 
 	if (!list)
 		return (NULL);
-	for (tmpList = list, jSum = 0; jSum < jStep; jSum++)
-		tmpList = tmpList->next;
-	for (idxList = list; jSum < size; tmpList = tmpList->next, jSum++)
+	for (tmpList = list; tmpList->index < jStep; tmpList = tmpList->next)
+		;
+	for (idxList = list; tmpList; tmpList = tmpList->next)
 	{
-		if (jSum % jStep == 0 || jSum == (size - 1))
-			printf("Value checked at index [%lu] = [%d]\n", jSum, tmpList->n);
-		if ((jSum >= size || tmpList->n > value) && (jSum % jStep == 0))
+		if (tmpList->index % jStep == 0 || tmpList->index == size - 1)
+			printf("Value checked at index [%lu] = [%d]\n", tmpList->index, tmpList->n);
+		if (tmpList->n >= value && (tmpList->index % jStep) == 0)
 			break;
-		idxList = idxList->next;
+		if (tmpList->index % jStep == 0)
+			idxList = tmpList;
 	}
-	if (jSum > size - 1)
-	{
-		jSum = size;
-		a = 1;
-	}
+	if (tmpList)
+		outIdx = tmpList->index;
+	else
+		outIdx = size - 1;
 	printf("Value found between indexes [%lu] and [%lu]\n",
-	       jSum - jStep, jSum - a);
-	for (idx = jSum - jStep; idx < jSum; idx++, idxList = idxList->next)
+	       idxList->index, outIdx);
+	for (; idxList && idxList->index <= outIdx; idxList = idxList->next)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", idx, idxList->n);
+		printf("Value checked at index [%lu] = [%d]\n", idxList->index, idxList->n);
 		if (idxList->n == value)
 			return (idxList);
 	}
